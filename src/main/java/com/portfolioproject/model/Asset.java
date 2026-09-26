b524ac295dc812d61ebb86a39c737c7c611c5919
Sailaja <sailaja.35612@gmail.com>
2026-09-26 11:17:53 +0530

    Added Main Asset Stock User Holding PortfolioService JsonUtil MutualFund classes

--- src/main/java/com/portfolioproject/model/Asset.java
+++ src/main/java/com/portfolioproject/model/Asset.java
@@ -1,22 +1,35 @@
 package com.portfolioproject.model;
 
-public abstract class Asset 
-{
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-	private String assetId;
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Stock.class, name = "stock"),
+        @JsonSubTypes.Type(value = MutualFund.class, name = "mutualFund")
+})
+public abstract class Asset {
+
+    private String assetId;
     private String assetName;
     private double purchasePrice;
 
-    // Constructor
-    public Asset(String assetId, String assetName, double purchasePrice) {
+    // Default constructor - required for Jackson
+    public Asset() {
+    }
 
+    // Parameterized constructor
+    public Asset(String assetId, String assetName, double purchasePrice) {
         this.assetId = assetId;
         this.assetName = assetName;
         this.purchasePrice = purchasePrice;
     }
 
     // Getters
-
     public String getAssetId() {
         return assetId;
     }
@@ -29,6 +42,20 @@ public abstract class Asset
         return purchasePrice;
     }
 
+    // Setters - required for Jackson
+    public void setAssetId(String assetId) {
+        this.assetId = assetId;
+    }
+
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
     // Abstract method
     public abstract double calculateCurrentValue();
 }
+ 
--- src/main/java/com/portfolioproject/model/Stock.java
+++ src/main/java/com/portfolioproject/model/Stock.java
@@ -1,24 +1,27 @@
 package com.portfolioproject.model;
 
-public class Stock extends Asset
-{
-	private double currentPrice;
+public class Stock extends Asset {
 
-    // Constructor
+    private double currentPrice;
+
+    // Default constructor - required for Jackson
+    public Stock() {
+        super();
+    }
+
+    // Parameterized constructor
     public Stock(String assetId,
                  String assetName,
                  double purchasePrice,
                  double currentPrice) {
 
         super(assetId, assetName, purchasePrice);
-
         this.currentPrice = currentPrice;
     }
 
     // Method overriding
     @Override
     public double calculateCurrentValue() {
-
         return currentPrice;
     }
 
@@ -29,13 +32,11 @@ public class Stock extends Asset
 
     // Setter
     public void setCurrentPrice(double currentPrice) {
-
         this.currentPrice = currentPrice;
     }
 
     @Override
     public String toString() {
-
         return "Stock{" +
                 "Asset ID='" + getAssetId() + '\'' +
                 ", Asset Name='" + getAssetName() + '\'' +
